97dc13386a99896f050bf2f8b065f0aad0815fce
Henri Buyse <henri.buyse@gmail.com>
2025-09-30 16:31:15 +0200

    feat(scripts): try to download HTML

--- scripts/ffvb-get-calendars.py
+++ scripts/ffvb-get-calendars.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python3
 
+import niquests
+from bs4 import BeautifulSoup
+
 URL = "https://www.ffvbbeach.org/ffvbapp/resu/vbspo_calendrier.php?saison=2025%2F2026"
 MATCHES = {
     "departemental-masculins": f"{URL}&codent=PTIDF94&poule=ARM&calend=COMPLET&equipe=20&x=9&y=10",
@@ -7,3 +10,9 @@ MATCHES = {
     "regional-masculins": f"{URL}&codent=PTIDF94&poule=PAM&calend=COMPLET&equipe=4&x=6&y=12",
     "regional-feminines": f"{URL}&codent=PTIDF94&poule=PAF&calend=COMPLET&equipe=2&x=12&y=8",
 }
+
+for name, url in MATCHES.items():
+    result = niquests.get(url)
+
+    with open(f"{name}.html", "w") as fd:
+        fd.write(result.text)
